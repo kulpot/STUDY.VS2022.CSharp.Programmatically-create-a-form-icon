6a3d4fd837e4c84753cbcc048fcc609f1e9a699a
Kulpot <kulpotwahu@gmail.com>
2023-06-03 10:29:22 +0800

    Add project files.

---
+++ Programmatically create a form icon/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace Programmatically_create_a_form_icon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Programmatically create a form icon/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//ref link:https://www.youtube.com/watch?v=Lu2Wq0UJrn8&list=PLAIBPfq19p2EJ6JY0f5DyQfybwBGVglcR&index=65
+// Creating a 16x16 icon for your form
+
+namespace Programmatically_create_a_form_icon
+{
+    public partial class Form1 : Form
+    {
+        private bool _showInner;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetIcon()
+        {
+            // Icon displays at 16x16
+            Bitmap canvas = new Bitmap(16, 16);
+
+            using(Graphics GFX = Graphics.FromImage(canvas))
+            {
+                // Increase quality
+                //GFX.SmoothingMode = SmoothingMode.AntiAlias;
+                GFX.CompositingQuality = CompositingQuality.HighQuality;
+
+                GFX.FillEllipse(Brushes.YellowGreen, 0, 0, canvas.Width, canvas.Height);
+
+                if((_showInner = !_showInner))
+                {
+                    GFX.FillEllipse(Brushes.Gray, 5, 5, canvas.Width - 5, canvas.Height - 5);
+                }
+
+                this.Icon = Icon.FromHandle(canvas.GetHicon());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SetIcon();
+        }
+    }
+}
